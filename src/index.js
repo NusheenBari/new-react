ca3dfaebd4aa99b12b4f50e5f76deaf472cd9293
Your Name <you@example.com>
2020-10-20 16:50:37 +0530

    thrid commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import ReactDOM from 'react-dom';
    return(
      <div>
        <h1>HEllo!</h1>
+       <p>World of ka-a-h</p>
      </div>
+       
    )
  }
+
+//  const Greet = () => {
+//     return React.createElement('h1', {}, 'Hello World')
+//  }
 
  ReactDOM.render(<App/>, document.getElementById('root'))
